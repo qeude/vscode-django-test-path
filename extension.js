2d07310e2f0a1061141abc849c641bce95b9f0ea
qeude <quentineude@gmail.com>
2020-10-12 13:23:03 +0200

    ♻️ Refactored implementation

--- extension.js
+++ extension.js
@@ -1,7 +1,11 @@
-const vscode = require('vscode');
-const fs = require('fs');
-const clipboardy = require('clipboardy');
-const path = require('path');
+const vscode = require("vscode");
+const fs = require("fs");
+const clipboardy = require("clipboardy");
+const path = require("path");
+
+function getTestCommandFromConfiguration() {
+  return vscode.workspace.getConfiguration("djangoTestPath").get("testCommand");
+}
 
 function getPythonPath(filePath) {
   const splittedPath = filePath.split(path.sep);
@@ -29,108 +33,111 @@ function getPythonPath(filePath) {
   return pythonPath.join(".");
 }
 
-function copyDjangoTestPath(uri) {
-  try {
-    const filePath = uri
-      ? uri.fsPath
-      : vscode.window.activeTextEditor.document.fileName;
-    const pythonPath = getPythonPath(filePath);
-    const selections = vscode.window.activeTextEditor.selections
-      .map(s => {
-        let start = s.start
-        if (vscode.window.activeTextEditor.document.lineAt(start.line).text.slice(start.character-4, start.character-1) === "def") {
-          let className = vscode.window.activeTextEditor.document.getText().split('\n').slice(0, start.line).reverse().find(s => s.match("(:?^|\s)class")).split(' ')[1].split('(')[0]
-          return className+"."+ vscode.window.activeTextEditor.document.getText(s)
-        } else if (vscode.window.activeTextEditor.document.lineAt(start.line).text.slice(start.character-6, start.character-1) === "class") {
-          return vscode.window.activeTextEditor.document.getText(s)
-        }
-      })
-      .filter(s => {
-        return !!s && !s.includes("\n") && !s.trim().includes(" ")
-      });
-    if (pythonPath && selections.length > 0) {
-      const pathStatement = generatePythonPathWithSelection(pythonPath, selections);
-      clipboardy.writeSync(pathStatement);
-    }
-    if (pythonPath && selections.length == 0) {
-      clipboardy.writeSync(pythonPath);
-    }
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-function copyDjangoTestCommand(uri) {
-  try {
-    let testCommand = vscode.workspace.getConfiguration('djangoTestPath').get('testCommand')
-    const filePath = uri
-      ? uri.fsPath
-      : vscode.window.activeTextEditor.document.fileName;
-    const pythonPath = getPythonPath(filePath);
-    const selections = vscode.window.activeTextEditor.selections
-      .map(s => {
-        let start = s.start
-        if (vscode.window.activeTextEditor.document.lineAt(start.line).text.slice(start.character-4, start.character-1) === "def") {
-          let className = vscode.window.activeTextEditor.document
+function parseSelections() {
+  return vscode.window.activeTextEditor.selections
+    .map((s) => {
+      let start = s.start;
+      if (
+        vscode.window.activeTextEditor.document
+          .lineAt(start.line)
+          .text.slice(start.character - 4, start.character - 1) === "def"
+      ) {
+        let className = vscode.window.activeTextEditor.document
           .getText()
-          .split('\n')
+          .split("\n")
           .slice(0, start.line)
           .reverse()
-          .find(s => s.match("(:?^|\s)class")).split(' ')[1].split('(')[0]
-          return className+"."+ vscode.window.activeTextEditor.document.getText(s)
-        } else if (vscode.window.activeTextEditor.document.lineAt(start.line).text.slice(start.character-6, start.character-1) === "class") {
-          return vscode.window.activeTextEditor.document.getText(s)
-        }
-      })
-      .filter(s => {
-        return !!s && !s.includes("\n") && !s.trim().includes(" ")
-      });
-    if (pythonPath && selections.length > 0) {
-      const pathStatement = generatePythonCommandWithSelection(pythonPath, selections);
-      clipboardy.writeSync(pathStatement);
-    }
-    if (pythonPath && selections.length == 0) {
-      clipboardy.writeSync(`${testCommand} ${pythonPath}`);
-    }
+          .find((s) => s.match("(:?^|s)class"))
+          .split(" ")[1]
+          .split("(")[0];
+        return (
+          className + "." + vscode.window.activeTextEditor.document.getText(s)
+        );
+      } else if (
+        vscode.window.activeTextEditor.document
+          .lineAt(start.line)
+          .text.slice(start.character - 6, start.character - 1) === "class"
+      ) {
+        return vscode.window.activeTextEditor.document.getText(s);
+      }
+    })
+    .filter((s) => {
+      return !!s && !s.includes("\n") && !s.trim().includes(" ");
+    });
+}
+
+function generatePath(pythonPath, selections, withCommand = false) {
+  if (pythonPath && selections.length > 0) {
+    return generatePathWithSelection(pythonPath, selections, withCommand);
+  }
+  if (pythonPath && selections.length == 0) {
+    return generatePathWithoutSelection(pythonPath, withCommand);
+  }
+}
+
+function generatePathWithSelection(
+  pythonPath,
+  selections,
+  withCommand = false
+) {
+  let testCommand = getTestCommandFromConfiguration();
+  if (selections.length == 0) {
+    return withCommand ? `${testCommand} ${pythonPath}` : `${pythonPath}`;
+  } else if (selections.length == 1) {
+    const selection = selections[0].trim();
+    return withCommand
+      ? `${testCommand} ${pythonPath}.${selection}`
+      : `${pythonPath}.${selection}`;
+  }
+  if (withCommand) {
+    return selections.map((s) => `${pythonPath}.${s.trim()}`).join(" ");
+  } else {
+    return `${testCommand} ${selections
+      .map((s) => `${pythonPath}.${s.trim()}`)
+      .join(" ")}`;
+  }
+}
+
+function generatePathWithoutSelection(pythonPath, withCommand = false) {
+  let testCommand = getTestCommandFromConfiguration();
+  return withCommand ? `${testCommand} ${pythonPath}` : `${pythonPath}`;
+}
+
+function copyDjangoTestPath(uri, withCommand) {
+  try {
+    const filePath = uri
+      ? uri.fsPath
+      : vscode.window.activeTextEditor.document.fileName;
+    const pythonPath = getPythonPath(filePath);
+    const selections = parseSelections();
+    const pathStatement = withCommand
+      ? generatePath(pythonPath, selections, true)
+      : generatePath(pythonPath, selections, false);
+    clipboardy.writeSync(pathStatement);
   } catch (e) {
     console.log(e);
   }
 }
 
-function generatePythonCommandWithSelection(pythonPath, selections) {
-  let testCommand = vscode.workspace.getConfiguration('djangoTestPath').get('testCommand')
-  if (selections.length == 0) {
-    return `${testCommand} ${pythonPath}`;
-  } else if (selections.length == 1) {
-    const selection = selections[0].trim();
-    return `${testCommand} ${pythonPath}.${selection}`;
-  }
-  const selection = `${testCommand} ${selections.map(s => `${pythonPath}.${s.trim()}`).join(" ")}`;
-  return selection;
+function copyDjangoTestPathWithoutCommand(uri) {
+  copyDjangoTestPath(uri, false);
 }
 
-function generatePythonPathWithSelection(pythonPath, selections) {
-  if (selections.length == 0) {
-    return `${pythonPath}`;
-  } else if (selections.length == 1) {
-    const selection = selections[0].trim();
-    return `${pythonPath}.${selection}`;
-  }
-  const selection = selections.map(s => `${pythonPath}.${s.trim()}`).join(" ");
-  return selection;
+function copyDjangoTestPathWithCommand(uri) {
+  copyDjangoTestPath(uri, true);
 }
 
 function activate(context) {
-  let djangoTestPath = vscode.commands.registerCommand(
-    "extension.copyDjangoTestPath",
-    copyDjangoTestPath
+  let djangoTestPathWithoutCommand = vscode.commands.registerCommand(
+    "extension.copyDjangoTestPathWithCommand",
+    copyDjangoTestPathWithCommand
   );
-  let djangoTestCommand = vscode.commands.registerCommand(
-    "extension.copyDjangoTestCommand",
-    copyDjangoTestCommand
-  );  
-  context.subscriptions.push(djangoTestPath);
-  context.subscriptions.push(djangoTestCommand);
+  let djangoTestPathWithCommand = vscode.commands.registerCommand(
+    "extension.copyDjangoTestPathWithoutCommand",
+    copyDjangoTestPathWithoutCommand
+  );
+  context.subscriptions.push(djangoTestPathWithoutCommand);
+  context.subscriptions.push(djangoTestPathWithCommand);
 }
 
 exports.activate = activate;
